4c74083457e253100054e331333c18331dfff254
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-02-18 16:49:55 +0000

    Update maincombat.lua

--- maincombat.lua
+++ maincombat.lua
@@ -76,6 +76,7 @@ while true do
 					local npctorso = closest[1].Torso
 					if (npctorso.Position - hrp.Position).Magnitude <= Configs.AttackRange and npchum.Health > 0 then
 						hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + CFrame.new(att.WorldPosition,(npctorso.Position + npchum.MoveDirection * npchum.WalkSpeed * lplr:GetNetworkPing()) * Vector3.new(1,0,1) + att.WorldPosition * Vector3.new(0,1,0)).LookVector)
+						npchrp:BreakJoints()
 						npchrp.Position = att.WorldPosition
 						coroutine.wrap(slash)(Tool)
 					end
