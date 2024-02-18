20c210ad11a4313be859353dfffb246dc5815cc6
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-02-18 12:08:38 +0000

    Update maincombat.lua

--- maincombat.lua
+++ maincombat.lua
@@ -18,41 +18,44 @@ MainCheats:CreateSlider("Range", 0, 100, 0, false, function(value)
 end)
 
 local lplr = game.Players.LocalPlayer
+
 while true do
-	if Configs.AutoPlayerLock then
-		local lplrchar = lplr.Character or lplr.CharacterAdded:Wait()
-		local hrp = lplrchar:WaitForChild("HumanoidRootPart")
+	local lplrchar = lplr.Character or lplr.CharacterAdded:Wait()
+	local hrp = lplrchar:WaitForChild("HumanoidRootPart")
+	
+	local att = Instance.new("Attachment", hrp)
+	att.Position = Vector3.new(1.5, 0.5, -1.5)
 
-		local closest
+	local closest
 
-		local detectnearest = Instance.new("Part", workspace)
-		detectnearest.Anchored = true
-		detectnearest.Size = Vector3.new(Configs.Range*2,Configs.Range*2,Configs.Range*2)
-		detectnearest.Shape = Enum.PartType.Ball
-		detectnearest.Position = hrp.Position
+	local detectnearest = Instance.new("Part", workspace)
+	detectnearest.Anchored = true
+	detectnearest.Size = Vector3.new(16,16,16)
+	detectnearest.Shape = Enum.PartType.Ball
+	detectnearest.Position = hrp.Position
 
-		local tp = detectnearest:GetTouchingParts()
-		detectnearest:Destroy()
+	local tp = detectnearest:GetTouchingParts()
+	detectnearest:Destroy()
 
-		for _, i in ipairs(tp) do
-			local char = i:FindFirstAncestorOfClass("Model")
-			if char and char ~= lplrchar then
-				local npchum = char:FindFirstChildOfClass("Humanoid")
-				local npchrp = char:FindFirstChild("HumanoidRootPart")
-				if npchum and npchrp then
-					local distance = (npchrp.Position - hrp.Position).Magnitude
-					if not closest or distance < closest[2] then
-						closest = {char,distance}
-					end
+	for _, i in ipairs(tp) do
+		local char = i:FindFirstAncestorOfClass("Model")
+		if char and char ~= lplrchar then
+			local npchum = char:FindFirstChildOfClass("Humanoid")
+			local npchrp = char:FindFirstChild("HumanoidRootPart")
+			if npchum and npchrp then
+				local distance = (npchrp.Position - hrp.Position).Magnitude
+				if (not closest or distance < closest[2]) and distance >= 0.5 then
+					closest = {char,distance}
 				end
 			end
 		end
-
-		if closest then
-			local npchum = closest[1].Humanoid
-			local npchrp = closest[1].HumanoidRootPart
-			hrp.CFrame = CFrame.new(hrp.Position + Vector3.new(math.random(-100,100)/1000,0,math.random(-100,100)/1000),(npchrp.Position + Vector3.new(math.random(-100,100)/1000,0,math.random(-100,100)/1000) + npchum.MoveDirection * npchum.WalkSpeed * 0.2) * Vector3.new(1,0,1) + hrp.Position * Vector3.new(0,1,0)) * CFrame.Angles(0,math.rad(12.5),0)
-		end
 	end
+
+	if closest then
+		local npchum = closest[1].Humanoid
+		local npchrp = closest[1].HumanoidRootPart
+		hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + CFrame.new(att.WorldPosition,npchrp.Position * Vector3.new(1,0,1) + att.WorldPosition * Vector3.new(0,1,0)).LookVector)
+	end
+	att:Destroy()
 	task.wait()
 end
