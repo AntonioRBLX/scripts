12bba506924743bc6d5c0d24c654304aafc61d3c
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-02-18 13:31:46 +0000

    Update maincombat.lua

--- maincombat.lua
+++ maincombat.lua
@@ -1,7 +1,8 @@
 local HttpService = game:GetService("HttpService")
 local Configs = {
-	AutoPlayerLock = false;
-	Range = 16;
+	AutoPlayerLock = true;
+	LockRange = 16;
+	AttackRange = 12;
 }
 local Library = loadstring(HttpService:GetAsync("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
@@ -13,51 +14,73 @@ MainCheats:CreateToggle("Auto-Player Lock", function(value)
 	Configs.AutoPlayerLock = value
 end)
 
-MainCheats:CreateSlider("Range", 0, 100, 0, false, function(value)
-	Configs.Range = value
+MainCheats:CreateSlider("Lock Range", 0, 100, 16, false, function(value)
+	Configs.LockRange = value
+end)
+
+MainCheats:CreateSlider("Attack Range", 0, 100, 12, false, function(value)
+	Configs.AttackRange = value
 end)
 
 local lplr = game.Players.LocalPlayer
+local mouse = lplr:GetMouse()
 
+function slash(tool)
+	if mouse.Icon ~= "rbxasset://textures/GunWaitCursor.png" then
+		print("slash")
+		tool:Activate()
+		task.wait(0.01)
+		tool:Activate()
+		task.wait(0.049)
+	end
+end
 while true do
 	if Configs.AutoPlayerLock then
-		local lplrchar = lplr.Character or lplr.CharacterAdded:Wait()
-		local hrp = lplrchar:WaitForChild("HumanoidRootPart")
-		
-		local att = Instance.new("Attachment", hrp)
-		att.Position = Vector3.new(1.5, 0.5, -1.5)
+		local lplrchar = lplr.Character
+		if lplrchar then
+			local Tool = lplrchar:FindFirstChildOfClass("Tool")
+			local hrp = lplrchar:FindFirstChild("HumanoidRootPart")
+			if Tool and hrp then
+				local att = Instance.new("Attachment", hrp)
+				att.Position = Vector3.new(1.5, 0.5, -1.5)
 
-		local closest
+				local closest
 
-		local detectnearest = Instance.new("Part", workspace)
-		detectnearest.Anchored = true
-		detectnearest.Size = Vector3.new(Configs.Range*2,Configs.Range*2,Configs.Range*2)
-		detectnearest.Shape = Enum.PartType.Ball
-		detectnearest.Position = hrp.Position
+				local detectnearest = Instance.new("Part", workspace)
+				detectnearest.Anchored = true
+				detectnearest.Size = Vector3.new(Configs.LockRange*2,Configs.LockRange*2,Configs.LockRange*2)
+				detectnearest.Shape = Enum.PartType.Ball
+				detectnearest.Position = hrp.Position
 
-		local tp = detectnearest:GetTouchingParts()
-		detectnearest:Destroy()
+				local tp = detectnearest:GetTouchingParts()
+				detectnearest:Destroy()
 
-		for _, i in ipairs(tp) do
-			local char = i:FindFirstAncestorOfClass("Model")
-			if char and char ~= lplrchar then
-				local npchum = char:FindFirstChildOfClass("Humanoid")
-				local npchrp = char:FindFirstChild("HumanoidRootPart")
-				if npchum and npchrp then
-					local distance = (npchrp.Position - hrp.Position).Magnitude
-					if (not closest or distance < closest[2]) and distance >= 0.5 then
-						closest = {char,distance}
+				for _, i in ipairs(tp) do
+					local char = i:FindFirstAncestorOfClass("Model")
+					if char and char ~= lplrchar then
+						local npchum = char:FindFirstChildOfClass("Humanoid")
+						local npchrp = char:FindFirstChild("HumanoidRootPart")
+						if npchum and npchrp then
+							local distance = (npchrp.Position - hrp.Position).Magnitude
+							if (not closest or distance < closest[2]) and distance >= 0.5 then
+								closest = {char,distance}
+							end
+						end
 					end
 				end
+
+				if closest then
+					local npchum = closest[1].Humanoid
+					local npchrp = closest[1].HumanoidRootPart
+					if (npchrp.Position - hrp.Position).Magnitude <= Configs.AttackRange and npchum.Health > 0 then
+						hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + CFrame.new(att.WorldPosition,(npchrp.Position + (npchum.MoveDirection * npchum.WalkSpeed) * lplr:GetNetworkPing()) * Vector3.new(1,0,1) + att.WorldPosition * Vector3.new(0,1,0)).LookVector)
+						coroutine.wrap(slash)(Tool)
+					end
+				end
+				
+				att:Destroy()
 			end
 		end
-
-		if closest then
-			local npchum = closest[1].Humanoid
-			local npchrp = closest[1].HumanoidRootPart
-			hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + CFrame.new(att.WorldPosition,(npchrp.Position + (npchrp.MoveDirection * npchrp.WalkSpeed) * lplr:GetNetworkPing()) * Vector3.new(1,0,1) + att.WorldPosition * Vector3.new(0,1,0)).LookVector)
-		end
-		att:Destroy()
 	end
 	task.wait()
 end
