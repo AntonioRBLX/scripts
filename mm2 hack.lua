90775ddfba08cb3c0d758717eb17e5d180f7e997
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-03-03 15:57:19 +0000

    Update mm2 hack.lua

--- mm2 hack.lua
+++ mm2 hack.lua
@@ -177,11 +177,9 @@ mt.__namecall = newcclosure(function(self,...)
 
 				if aimpos then
 					args[2] = aimpos
-					return self.InvokeServer(self,table.unpack(args))
 				end
-				return namecall(self,...)
 			end
-			return namecall(self,...)
+			return self.InvokeServer(self,table.unpack(args))
 		elseif configs.KnifeAimbot and tostring(self) == "Throw" and tostring(method) == "FireServer" then
 			local closest = GetClosestPlayer(configs.FOV,500)
 			if closest then
@@ -193,13 +191,10 @@ mt.__namecall = newcclosure(function(self,...)
 
 				if aimpos then
 					args[1] = CFrame.new(aimpos,aimpos)
-					return self.FireServer(self,table.unpack(args))
 				end
-				return namecall(self,...)
 			end
-			return namecall(self,...)	
+			return self.FireServer(self,table.unpack(args))
 		end
-		return namecall(self,...)
 	end
 	return namecall(self,...)
 end)
