7f975afed6fa912b05d384872ff46ee2e7a08dcd
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-03-17 16:59:12 +0000

    Create blade ball gui.lua

---
+++ blade ball gui.lua
@@ -0,0 +1,107 @@
+repeat task.wait() until game.Players.LocalPlayer
+
+local replicatedstorage = game:GetService("ReplicatedStorage")
+
+local remotes = replicatedstorage:WaitForChild("Remotes")
+local parrybuttonpress = remotes:WaitForChild("ParryButtonPress")
+local abilitybuttonpress = remotes:WaitForChild("AbilityButtonPress")
+
+local ballsfolder = workspace:WaitForChild("Balls")
+local alivefolder = workspace:WaitForChild("Alive")
+
+local configs = {
+	AutoParry = false;
+	RagingAndRaptures = false;
+	MaxAngle = 135;
+	ShowCalculations = false;
+}
+
+local lplr = game.Players.LocalPlayer
+local char = lplr.Character or lplr.CharacterAdded:Wait()
+local balls = {}
+local closestball
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local BladeBallWindow = library:NewWindow("Blade Ball")
+local MainSection = BladeBallWindow:NewSection("Main")
+
+MainSection:CreateToggle("Auto Parry", function(value)
+	configs.AutoParry = value
+end)
+MainSection:CreateToggle("Raging & Raptures", function(value)
+	configs.RagingAndRaptures = value
+end)
+MainSection:CreateSlider("Slider", 0, 360, configs.MaxAngle, false, function(value)
+	configs.MaxAngle = value
+end)
+MainSection:CreateToggle("Show Calculations", function(value)
+	configs.ShowCalculations = value
+end)
+while true do
+	table.clear(balls)
+	closestball = nil
+	local playergui = lplr:FindFirstChild("PlayerGui")
+	
+	local char = lplr.Character
+	if char and char.Parent == alivefolder then
+		local hrp = char:FindFirstChild("HumanoidRootPart")
+		if hrp then
+			for _, child in pairs(ballsfolder:GetChildren()) do
+				if closestball:IsA("Part") and child:GetAttribute("realBall") then
+					table.insert(balls,child)
+					local dist = (child.Position - hrp.Position).Magnitude
+					if not closestball or dist < (hrp.Position - closestball.Position).Magnitude then
+						closestball = child
+					end
+				end
+			end
+			if closestball then
+				if configs.AutoParry and closestball.BrickColor == BrickColor.new("Really red") then
+					local parrysize = closestball.Velocity.Magnitude * stats().PerformanceStats.Ping:GetValue() / 1000
+					
+					if (hrp.Position - closestball.Position).Magnitude >= parrysize then
+						local x,y,z = CFrame.new(closestball.Position,hrp.Position):ToEulerAnglesYXZ()
+						local aim_target_look = Vector3.new(x,y,z)
+						
+						local x,y,z = CFrame.new(closestball.Position,closestball.Position + closestball.Velocity.Unit):ToEulerAnglesYXZ()
+						local aim_velocityunit_look = Vector3.new(x,y,z)
+
+						local diff = aim_target_look - aim_velocityunit_look
+						
+						if math.deg(diff.X) <= configs.MaxAngle and math.deg(diff.X) >= -configs.MaxAngle and math.deg(diff.Y) <= configs.MaxAngle and math.deg(diff.Y) >= -configs.MaxAngle then
+							local hotbar = playergui:FindFirstChild("Hotbar")
+							
+							if hotbar then
+								local Block = hotbar:FindFirstChild("Block")
+								
+								if configs.RagingAndRaptures then
+									local Ability = hotbar:FindFirstChild("Ability")
+									local Abilities = char:FindFirstChild("Abilities")
+									
+									if Ability and Abilities and (Abilities["Raging Deflection"].Enabled or Abilities["Rapture"].Enabled) then
+										local AbilityUIGradient = Ability:FindFirstChildOfClass("UIGradient")
+										
+										if AbilityUIGradient and AbilityUIGradient.Offset.Y <= 0.5 then
+											abilitybuttonpress:Fire()
+										end
+									end
+								elseif Block then
+									local BlockUIGradient = Block:FindFirstChildOfClass("UIGradient")
+									
+									if BlockUIGradient and BlockUIGradient.Offset.Y <= 0.5 then
+										parrybuttonpress:Fire()
+									end
+								end
+							end
+						end
+					end
+				end
+				if configs.ShowCalculations then
+					
+				end
+			end
+		end
+	end
+	task.wait()
+end
