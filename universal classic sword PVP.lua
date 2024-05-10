510de050eac98b71c04a05f99389810bc7b9850b
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-05-10 20:04:54 +0100

    Update universal classic sword PVP.lua

--- universal classic sword PVP.lua
+++ universal classic sword PVP.lua
@@ -23,6 +23,9 @@ MainCheats:CreateSlider("Attack Range", 0, 100, 12, false, function(value)
 end)
 
 local lplr = game.Players.LocalPlayer
+local lplrchar = lplr.Character or lplr.CharacterAdded:Wait()
+local lplrhrp = lplrchar:WaitForChild("HumanoidRootPart")
+
 local mouse = lplr:GetMouse()
 
 function slash(tool)
@@ -34,57 +37,54 @@ function slash(tool)
 		task.wait(0.049)
 	end
 end
+lplr.CharacterAdded:Connect(function(char)
+	lplrchar = char
+	lplrhrp = lplrchar:WaitForChild("HumanoidRootPart")
+end)
 while true do
-	local npchrp
-	if Configs.AutoPlayerLock then
-		local lplrchar = lplr.Character
-		if lplrchar then
-			local Tool = lplrchar:FindFirstChildOfClass("Tool")
-			local hrp = lplrchar:FindFirstChild("HumanoidRootPart")
-			if Tool and hrp then
-				local att = Instance.new("Attachment", hrp)
-				att.Position = Vector3.new(1.5, 0.5, -1.5)
+	if lplrchar and lplrhrp then
+		local Tool = lplrhrp:FindFirstChildOfClass("Tool")
+		local att = Instance.new("Attachment", lplrhrp)
+		att.Position = Vector3.new(1.5, 0.5, -1.5)
 
-				local closest
+		local closest
 
-				local detectnearest = Instance.new("Part", workspace)
-				detectnearest.Anchored = true
-				detectnearest.Size = Vector3.new(Configs.LockRange*2,Configs.LockRange*2,Configs.LockRange*2)
-				detectnearest.Shape = Enum.PartType.Ball
-				detectnearest.Position = hrp.Position
+		local detectnearest = Instance.new("Part", workspace)
+		detectnearest.Anchored = true
+		detectnearest.Size = Vector3.new(Configs.LockRange*2,Configs.LockRange*2,Configs.LockRange*2)
+		detectnearest.Shape = Enum.PartType.Ball
+		detectnearest.Position = lplrhrp.Position
 
-				local tp = detectnearest:GetTouchingParts()
-				detectnearest:Destroy()
+		local tp = detectnearest:GetTouchingParts()
+		detectnearest:Destroy()
 
-				for _, i in ipairs(tp) do
-					local char = i:FindFirstAncestorOfClass("Model")
-					if char and char ~= lplrchar then
-						local npchum = char:FindFirstChildOfClass("Humanoid")
-						local npchrp = char:FindFirstChild("HumanoidRootPart")
-						local npctorso = char:FindFirstChild("Torso")
-						if npchum and npchrp and npctorso then
-							local distance = (npctorso.Position - hrp.Position).Magnitude
-							if (not closest or distance < closest[2]) and distance >= 0.5 then
-								closest = {char,distance}
-							end
-						end
+		for _, i in ipairs(tp) do
+			local char = i:FindFirstAncestorOfClass("Model")
+			if char and char ~= lplrchar then
+				local npchum = char:FindFirstChildOfClass("Humanoid")
+				local npchrp = char:FindFirstChild("HumanoidRootPart")
+				local npctorso = char:FindFirstChild("Torso")
+				if npchum and npchrp and npctorso then
+					local distance = (npctorso.Position - lplrhrp.Position).Magnitude
+					if (not closest or distance < closest[2]) and distance >= 0.5 then
+						closest = {char,distance}
 					end
 				end
-
-				if closest then
-					local npchum = closest[1].Humanoid
-					npchrp = closest[1].HumanoidRootPart
-					local npctorso = closest[1].Torso
-					if (npctorso.Position - hrp.Position).Magnitude <= Configs.AttackRange and npchum.Health > 0 then
-						hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + CFrame.new(att.WorldPosition,(npctorso.Position + npchum.MoveDirection * npchum.WalkSpeed * lplr:GetNetworkPing()) * Vector3.new(1,0,1) + att.WorldPosition * Vector3.new(0,1,0)).LookVector)
-						npchrp.Size = Vector3.new(100,100,100)
-						coroutine.wrap(slash)(Tool)
-					end
-				end
-
-				att:Destroy()
 			end
 		end
+
+		if closest then
+			local npchum = closest[1].Humanoid
+			npchrp = closest[1].HumanoidRootPart
+			local npctorso = closest[1].Torso
+			if (npctorso.Position - lplrhrp.Position).Magnitude <= Configs.AttackRange and npchum.Health > 0 then
+				lplrhrp.CFrame = CFrame.new(lplrhrp.Position,lplrhrp.Position + CFrame.new(att.WorldPosition,(npctorso.Position + npchum.MoveDirection * npchum.WalkSpeed * lplr:GetNetworkPing()) * Vector3.new(1,0,1) + att.WorldPosition * Vector3.new(0,1,0)).LookVector)
+				npchrp.Size = Vector3.new(100,100,100)
+				coroutine.wrap(slash)(Tool)
+			end
+		end
+
+		att:Destroy()
 	end
 	task.wait()
 	if npchrp then
