08a1ecc29d70cafcda71cee9d9bcfe744ff6fe69
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-06-15 22:54:24 +0100

    Update targetstrafe.lua

--- targetstrafe.lua
+++ targetstrafe.lua
@@ -1,63 +1,101 @@
 local UIS = game:GetService("UserInputService")
-local StarterGui = game:GetService("StarterGui")
 local RS = game:GetService("RunService")
+local SG = game:GetService("StarterGui")
 local Players = game:GetService("Players")
-
 local lplr = Players.LocalPlayer
-local lplrchar = lplr.Character or lplr.CharacterAdded:Wait()
-local lplrhum = lplrchar:WaitForChild("Humanoid")
-local lplrhrp = lplrchar:WaitForChild("HumanoidRootPart")
+local killerChar = lplr.Character or lplr.CharacterAdded:Wait()
+local killerHum = killerChar:WaitForChild("Humanoid")
+local killerHrp = killerChar:WaitForChild("HumanoidRootPart")
 
-local Controls = require(lplr.PlayerScripts.PlayerModule):GetControls()
-local enabled = false
+local MaxDist = 1000
+local pivotAngle = 0
+local pivotDirection = 1
+local pivotDistance = 3
+local prevtimer
 
 lplr.CharacterAdded:Connect(function(char)
-	lplrchar = char
-	lplrhrp = lplrchar:WaitForChild("HumanoidRootPart")
-	lplrhum = lplrchar:WaitForChild("Humanoid")
+	killerChar = char
+	killerHum = char:WaitForChild("Humanoid")
+	killerHrp = char:WaitForChild("HumanoidRootPart")
 end)
-UIS.InputBegan:Connect(function(input)
-	if input.KeyCode == Enum.KeyCode.E then
-		enabled = not enabled
-		StarterGui:SetCore("SendNotification", {
-			Title = "Info";
-			Text = "Auto-Strafe has been set to "..tostring(enabled);
-		})
-	end
-end)
-while true do
+
+local enabled = false
+
+function getClosest()
 	local closest
-	local closesthrp
-	local closesthum
-	local distance
-	for _, plr in pairs(Players:GetPlayers()) do
-		local char = plr.Character
-		if char then
-			local enemyhrp = char:FindFirstChild("HumanoidRootPart")
-			local enemyhum = char:FindFirstChildOfClass("Humanoid")
-			if enemyhrp and enemyhrp:IsA("BasePart") and enemyhum and enemyhum.Health > 0 then
-				local distancetemp = (enemyhrp.Position - lplrhrp.Position).Magnitude
-				if not closest or distancetemp < distance then
-					closest = char
-					closesthrp = enemyhrp
-					closesthum = enemyhum
-					distance = distancetemp
-				end
+	local dist
+	for _, child in pairs(workspace:GetChildren()) do
+		local NPCHum = child:FindFirstChildOfClass("Humanoid")
+		local NPCHrp = child:FindFirstChild("HumanoidRootPart")
+
+		if child ~= killerChar and NPCHum and NPCHum.Health > 0 and NPCHrp and NPCHrp:IsA("BasePart") then
+			local dist2 = (NPCHrp.Position - killerHrp.Position).Magnitude
+			if (not closest or dist2 < dist) and dist2 <= MaxDist then
+				closest = child
+				dist = dist2
 			end
 		end
 	end
-	if enabled and closest then
-		Controls:Disable()
-		local distance = (closesthrp.Position - lplrhrp.Position).Magnitude
-		local leadvector = closesthrp.Position + (closesthum.MoveDirection * closesthum.WalkSpeed) * ((distance - 1) / lplrhum.WalkSpeed)
-		local leaddistance = (leadvector - lplrhrp.Position).Magnitude
-		if leaddistance < 2 and distance < 6 then
-			lplrhum:Move(CFrame.new(lplrhrp,lplrhrp.Position + (lplrhrp.Position - closesthrp.Position)).LookVector)
-		else
-			lplrhum:Move(CFrame.new(lplrhrp.Position,leadvector).LookVector)
-		end
-	else
-		Controls:Enable()
-	end
-	task.wait()
+	return closest
 end
+UIS.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.E then
+		enabled = not enabled
+		SG:SetCore("SendNotification", {
+			Title = "Info",
+			Text = "Auto-Strafe has been set to "..tostring(enabled),
+		})
+	end
+end)
+RS.Stepped:Connect(function(timer)
+	if not enabled or not killerChar.Parent or not killerHum.Parent or killerHum.Health <= 0 or not killerHrp.Parent then return end
+	local closest = getClosest()
+	if math.random(1,21) == 1 then
+		pivotDistance = math.random(32,75)/10
+	end
+	if closest and prevtimer then
+		local enemyroot = closest.HumanoidRootPart
+		local enemypos = enemyroot.Position
+
+		local killerhrppos = killerHrp.Position
+		local killerhumvel = killerHrp.Velocity
+		
+		local dist = (killerhrppos - enemypos).Magnitude
+
+		local x,y,z = CFrame.new(enemypos,killerhrppos):ToEulerAnglesYXZ()
+		local aim_target_look = Vector3.new(x,y,z)
+
+		local x,y,z = CFrame.new(enemypos,enemypos + enemyroot.CFrame.LookVector):ToEulerAnglesYXZ()
+		local aim_cframeunit_look = Vector3.new(x,y,z)
+
+		local diff = aim_target_look - aim_cframeunit_look
+		if math.deg(diff.X) <= 45 and math.deg(diff.X) >= -45 and math.deg(diff.Y) <= 45 and math.deg(diff.Y) >= -45 then
+			local circumference = 2 * math.pi * pivotDistance
+			local duration = circumference / killerHum.WalkSpeed
+			pivotAngle += (1/duration * (timer - prevtimer)) * 360 * pivotDirection
+			if pivotAngle >= 360 then
+				pivotAngle = 0
+			end
+
+			local moveto = enemypos + CFrame.Angles(0,math.rad(pivotAngle),0).LookVector * pivotDistance
+			killerHum:MoveTo(moveto)
+			if killerHum:GetState() ~= Enum.HumanoidStateType.Freefall and math.random(1,7) then
+				pivotDirection = -pivotDirection
+				if math.random(1,2) == 1 then
+					killerHum.Jump = true
+				end
+			elseif killerHum:GetState() == Enum.HumanoidStateType.Freefall and killerhumvel.Y >= -2 and killerhumvel.Y <= 2 and math.random(1,3) == 1 then
+				pivotDirection = -pivotDirection
+			end
+		else
+			killerHum:MoveTo(enemypos)
+		end
+		if enemypos.Y > killerhrppos.Y + 1 then
+			killerHum.Jump = true
+		end
+		if math.random(1,21) == 1 then
+			killerHum.Jump = true
+		end
+	end
+	prevtimer = timer
+end)
