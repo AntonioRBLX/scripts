406f53dcbc08329f496045a9150384de6d55f8e9
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-07-07 20:17:45 +0100

    Update targetstrafe.lua

--- targetstrafe.lua
+++ targetstrafe.lua
@@ -1,234 +1,75 @@
--- {Services} --
-local UIS = game:GetService("UserInputService")
-local SG = game:GetService("StarterGui")
 local RS = game:GetService("RunService")
 local Players = game:GetService("Players")
-
--- {LocalPlayer} --
 local lplr = Players.LocalPlayer
-local lplrchar = lplr.Character or lplr.CharacterAdded:Wait()
-local lplrhum = lplrchar:WaitForChild("Humanoid")
-local lplrhrp = lplrchar:WaitForChild("HumanoidRootPart")
-local mouse = lplr:GetMouse()
-
-local Controls = require(lplr:WaitForChild("PlayerScripts"):WaitForChild("PlayerModule")):GetControls()
-
--- {Combat} --
--- Main
-local enabled = false
-
--- Combat Variables
-local behavior = 1 -- 1: Attack, 2: Evade, 3: Counter
-local pivotdistance = 3
+local playermodule = require(game.Players.LocalPlayer.PlayerScripts:WaitForChild("PlayerModule"))
+local controls = playermodule:GetControls()
+local char = lplr.Character or lplr.CharacterAdded:Wait()
+local hum = char:WaitForChild("Humanoid")
+local hrp = char:WaitForChild("HumanoidRootPart")
 local pivotangle = 0
 local pivotdirection = 1
-
--- Sword Tip
-local lplrswordtip
-
--- Zones
-local evasionzone
-local dangerzone
-local safetoenter = false
-
--- Enemy Variables
-local prevenemy
-local enemy
-local enemyhrp
-local enemyhum
-local enemyanimator
-local enemyla
-local enemyra
-local enemyattack
-
--- Network
-local ping = 0
-
--- Stepped Parameters
-local timer
-local prevtimer
-
--- {Functions} --
-lplr.CharacterAdded:Connect(function(char)
-	lplrchar = char
-	lplrhum = lplrchar:WaitForChild("Humanoid")
-	lplrhrp = lplrchar:WaitForChild("HumanoidRootPart")
+local pivotdistance = 7
+lplr.CharacterAdded:Connect(function(newchar)
+	char = newchar
+	hum = newchar:WaitForChild("Humanoid")
+	hrp = newchar:WaitForChild("HumanoidRootPart")
 end)
-UIS.InputBegan:Connect(function(input)
-	if input.KeyCode == Enum.KeyCode.E then
-		enabled = not enabled
-		SG:SetCore("SendNotification",{
-			Title = "Info", -- Required
-			Text = "Combat AI has been set to "..tostring(enabled),
-		})
-	end
-end)
-function attack()
-	if lplrchar and enemy and enemyhrp and enemyla and enemyra and lplrswordtip then
-		local Tool = lplrchar:FindFirstChildOfClass("Tool")
-		if Tool and mouse.Icon ~= "rbxasset://textures/GunWaitCursor.png" then
-			local CFrameLook = CFrame.new(lplrswordtip.WorldPosition,enemyhrp.Position * Vector3.new(1,0,1) + lplrswordtip.WorldPosition * Vector3.new(0,1,0))
-			lplrhrp.CFrame = CFrame.new(lplrhrp.Position,lplrhrp.Position + CFrameLook.LookVector)
-
-			Tool:Activate()
-			task.wait(0.01)
-			Tool:Activate()
-		end
-	end
-end
-function changepivotangle()
-	local circumference = 2 * math.pi * pivotdistance
-	local duration = circumference / lplrhum.WalkSpeed
-	pivotangle += (1/duration * (timer - prevtimer)) * 360 * pivotdirection
-	if pivotangle >= 360 then
-		pivotangle = 0
-	end
-end
-
--- {Loops} --
-RS.Stepped:Connect(function(_, t)
-	if not lplrchar or not lplrhrp or not lplrhrp.Parent or not lplrhum or not lplrhum.Parent or lplrhum.Health <= 0 then return end
-	ping = lplr:GetNetworkPing()
-	timer = t
-	if enabled then
-		local distance
-		enemy = nil
-		enemyhrp = nil
-		enemyhum = nil
-		enemyanimator = nil
-		enemyla = nil
-		enemyra = nil
-
-		for _, plr in pairs(Players:GetPlayers()) do
-			local char = plr.Character
-			if char and char ~= lplrchar then
-				local temphrp = char:FindFirstChild("Torso") or char:FindFirstChild("HumanoidRootPart")
-				local temphum = char:FindFirstChildOfClass("Humanoid")
-				local templa = char:FindFirstChild("Left Arm") or char:FindFirstChild("LeftUpperArm")
-				local tempra = char:FindFirstChild("Right Arm") or char:FindFirstChild("RightUpperArm")
-				if temphrp and temphum and temphum.Health > 0 and templa and tempra then
-					local tempanimator = temphum:FindFirstChildOfClass("Animator")
-					if tempanimator then
-						local distancetemp = (tempra.Position - lplrhrp.Position).Magnitude
-						if not enemy or distancetemp < distance then
-							enemy = char
-							distance = distancetemp
-							enemyhrp = temphrp
-							enemyhum = temphum
-							enemyanimator = tempanimator
-							enemyla = templa
-							enemyra = tempra
-						end
-					end
+function getClosest()
+	local closest
+	local closestdistance = math.huge
+	for i, v in ipairs(Players:GetPlayers()) do
+		local enemychar = v.Character
+		if v ~= lplr and enemychar then
+			local enemyhrp = enemychar:FindFirstChild("HumanoidRootPart")
+			local enemyhum = enemychar:FindFirstChildOfClass("Humanoid")
+			if enemyhum and enemyhum.Health > 0 and enemyhrp then
+				local distance = (enemyhrp.Position - hrp.Position).Magnitude
+				if distance < closestdistance then
+					closest = enemychar
+					closestdistance = distance
 				end
 			end
 		end
-		if enemy then
-			Controls:Disable()
-			if enemy ~= prevenemy then enemyattack = nil end
-			if enemyattack and tick() + ping - 0.8 > enemyattack then enemyattack = nil end
-			if not enemyattack then safetoenter = true end
-			behavior = 1
-
-			local dist = (enemyhrp.Position - lplrhrp.Position).Magnitude
-
-			if not lplrswordtip or not lplrswordtip.Parent then
-				local temp = Instance.new("Attachment", lplrhrp)
-				temp.Position = Vector3.new(1.5, 0.5, 0)
-				lplrswordtip = temp
-			end
-			if not evasionzone or not evasionzone.Parent then
-				local temp = Instance.new("Part", enemyhrp)
-				temp.Size = Vector3.new(13,13,13)
-				temp.Shape = Enum.PartType.Ball
-				evasionzone = temp
-			end
-			if not dangerzone or not dangerzone.Parent then
-				local temp = Instance.new("Part", enemyhrp)
-				temp.Size = Vector3.new(7,7,7)
-				temp.Shape = Enum.PartType.Ball
-				dangerzone = temp
-			end
-
-			if not enemyattack then
-				local anims = enemyanimator:GetPlayingAnimationTracks()
-				for _, anim in pairs(anims) do
-					if anim.Animation and (anim.Animation.AnimationId == "rbxassetid://522635514" or anim.Animation.AnimationId == "rbxassetid://522638767" or anim.Animation.AnimationId == "http://www.roblox.com/asset/?id=129967390" or anim.Animation.AnimationId == "http://www.roblox.com/asset/?id=129967478") then
-						safetoenter = false
-						enemyattack = tick()
-						break
-					end
+	end
+	return closest
+end
+RS.Stepped:Connect(function(_,delta)
+	local target = getClosest()
+	if target then
+		controls:Disable()
+		local targethrp = target.HumanoidRootPart
+		local targethum = target.Humanoid
+		local att = Instance.new("Attachment", hrp)
+		att.Position = Vector3.new(1.49,0,0)
+		local Look = CFrame.new(att.WorldPosition,targethrp.Position * Vector3.new(1,0,1) + att.WorldPosition * Vector3.new(0,1,0))
+		hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + Look.LookVector)
+		if math.random(1,20) == 1 then
+			pivotdistance = math.random(7,11)
+		end
+		if math.random(1,4) == 1 then
+			if math.random(1,2) == 1 then
+				pivotdirection = -pivotdirection
+				if math.random(1,3) == 1 then
+					hum.Jump = true
 				end
 			end
-
-			evasionzone.Size = Vector3.new(30+ping*2,30+ping*2,30+ping*2)
-			dangerzone.Size = Vector3.new(15+ping*2,15+ping*2,15+ping*2)
-
-			if not safetoenter then
-				dangerzone.Position = Vector3.new(99999,99999,99999)
-				evasionzone.Position = enemyhrp.Position
-				local evasionzoneinterceptingparts = evasionzone:GetTouchingParts()
-				for _, v in pairs(evasionzoneinterceptingparts) do
-					local char = v:FindFirstAncestorOfClass("Model")
-					if char and char == lplrchar then
-						behavior = 2
-						break
-					end
-				end
+		end
+		local circumference = 2 * math.pi * pivotdistance
+		pivotangle += 360 / (circumference / hum.WalkSpeed) * delta * pivotdirection
+		if pivotangle >= 360 or pivotangle <= -360 then
+			pivotangle = 0
+		end
+		local distance = (targethrp.Position - hrp.Position).Magnitude
+		local predictedpos = targethrp.Position + targethrp.Velocity * (distance / hum.WalkSpeed)
+		if distance <= 11 then
+			hum:MoveTo(predictedpos + CFrame.Angles(0,math.rad(pivotangle),0).LookVector * pivotdistance)
+			if distance <= 7 then
+				hum.Jump = true
 			end
-
-			dangerzone.Position = enemyhrp.Position
-			evasionzone.Position = Vector3.new(99999,99999,99999)
-			local dangerzoneinterceptingparts = dangerzone:GetTouchingParts()
-			dangerzone.Position = Vector3.new(99999,99999,99999)
-
-			for _, v in pairs(dangerzoneinterceptingparts) do
-				local char = v:FindFirstAncestorOfClass("Model")
-				if char and char == lplrchar then
-					behavior = 3
-					break
-				end
-			end
-
-			if behavior == 1 then
-				lplrhum:MoveTo(enemyhrp.Position + enemyhrp.Velocity * dist / lplrhum.WalkSpeed)
-				if dist <= 15 then
-					coroutine.wrap(attack)(enemy)
-				end
-			elseif behavior == 2 then
-				lplrhum.Jump = true
-				lplrhum:MoveTo(lplrhrp.Position + (lplrhrp.Position - enemyhrp.Position))
-			elseif behavior == 3 then
-				local x,y,z = CFrame.new(enemyhrp.Position,lplrhrp.Position):ToEulerAnglesYXZ()
-				local aim_target_look = Vector3.new(x,y,z)
-
-				local x,y,z = CFrame.new(enemyhrp.Position,enemyhrp.Position + enemyhrp.CFrame.LookVector):ToEulerAnglesYXZ()
-				local aim_cframelookvectorunit_look = Vector3.new(x,y,z)
-
-				local diff = aim_target_look - aim_cframelookvectorunit_look
-
-				if math.deg(diff.X) <= 60 and math.deg(diff.X) >= -60 and math.deg(diff.Y) <= 60 and math.deg(diff.Y) >= -60 or lplrhrp.Position.Y > enemyhrp.Position.Y + 1 then
-					local backstab = enemyhrp.Position + -enemyhrp.CFrame.LookVector * 5
-					local dist = (backstab - lplrhrp.Position).Magnitude
-					if dist > 1.6 then
-						lplrhum.Jump = true
-					else
-						coroutine.wrap(attack)(enemy)
-					end
-					lplrhum:MoveTo(backstab)
-				else
-					pivotdirection = -1
-					pivotdistance = 2
-					changepivotangle()
-					lplrhum:MoveTo(enemyhrp.Position + CFrame.Angles(0,math.rad(pivotangle),0).LookVector * pivotdistance)
-				end
-			end
-		else
-			Controls:Enable()
+		elseif distance <= 35 then
+			hum:MoveTo(predictedpos)
 		end
 	else
-		Controls:Enable()
+		controls:Enable()
 	end
-	prevtimer = t
-	prevenemy = enemy
 end)
